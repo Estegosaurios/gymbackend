6fa13d6732169a374a82530faafcb91aac05860d
sergiga <srgarcia91@gmail.com>
2018-10-06 20:39:37 +0200

    Make the workout extend from BaseModel

--- programs/models.py
+++ programs/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from core.models import BaseModel
 
 
-class Workout(models.Model):
+class Workout(BaseModel):
     name = models.CharField(max_length=100)
 
     def __str__(self):
