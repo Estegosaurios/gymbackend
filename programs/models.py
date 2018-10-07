433dc9b626f0d83ec6ce053ae6b3b72dea514171
Alex <alejalonta@gmail.com>
2018-10-07 12:08:03 +0200

    Created program and workoutinprogram models

--- programs/models.py
+++ programs/models.py
@@ -7,3 +7,29 @@ class Workout(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class Program(BaseModel):
+    name = models.CharField(max_lenght=100)
+    workouts = models.ManyToManyField(
+        Workout,
+        through='WorkoutInProgram',
+        through_fields=('program', 'workout'),
+        related_name='programs'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class WorkoutInProgram(BaseModel):
+    date = models.DateField()
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return u'{program}-{workout}. Date: {date}'.format(
+            program=self.program.name,
+            workout=self.workout.name,
+            date=self.date
+        )
