048db2c9fb61f72b4858094a7b90175ce291201f
Alex <alejalonta@gmail.com>
2018-10-07 13:58:17 +0200

    Added WorkoutInProgram end point

--- programs/serializers.py
+++ programs/serializers.py
@@ -1,14 +1,20 @@
 from rest_framework import serializers
-from programs.models import Workout, Program
+from programs.models import Workout, Program, WorkoutInProgram
 
 
 class WorkoutSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workout
-        fields = ('id', 'name', )
+        fields = ('id', 'name')
 
 
 class ProgramSerializer(serializers.ModelSerializer):
     class Meta:
         model = Program
         fields = ('id', 'name', 'workouts_in_program')
+
+
+class WorkoutInProgramSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkoutInProgram
+        fields = ('id', 'date', 'program', 'workout')
--- programs/views.py
+++ programs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from programs.models import Workout, Program
-from programs.serializers import WorkoutSerializer, ProgramSerializer
+from programs.models import Workout, Program, WorkoutInProgram
+from programs.serializers import (
+    WorkoutSerializer, ProgramSerializer, WorkoutInProgramSerializer
+)
 
 
 class WorkoutViewSet(viewsets.ModelViewSet):
@@ -12,3 +14,8 @@ class WorkoutViewSet(viewsets.ModelViewSet):
 class ProgramViewSet(viewsets.ModelViewSet):
     queryset = Program.objects.all()
     serializer_class = ProgramSerializer
+
+
+class WorkoutInProgramViewSet(viewsets.ModelViewSet):
+    queryset = WorkoutInProgram.objects.all()
+    serializer_class = WorkoutInProgramSerializer
